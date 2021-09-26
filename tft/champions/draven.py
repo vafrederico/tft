74cc4d44686c4b7cc7a28039bd247b2333d7844b
Vinícius Frederico <vafrederico@gmail.com>
2021-09-26 16:03:42 -0700

    Draven axe fix

--- tft/champions/draven.py
+++ tft/champions/draven.py
@@ -3,26 +3,35 @@ from tft.game.constant import GAME_LOOP
 from time import time
 from tft.stats import BaseChampStats
 from tft.champions.champion import BaseChampion
+from logging import getLogger
+
+log = getLogger(__name__)
 
 
 class Draven(BaseChampion):
-    AXE_RETURN_SECONDS = 1
+    AXE_RETURN_SECONDS = 1.5
     base_stats = BaseChampStats([700, 1260, 2268], [80, 144, 259],
                                 armor=30,
                                 armor_ignore=0.5,
                                 mr=30,
-                                aspd=0.8)
+                                aspd=0.8,
+                                mana=40)
     axes = []
 
-    def ult(self, target: 'BaseChampion') -> None:
-        self.axes.append(time())
+    def do_ult(self) -> None:
+        self.axes.insert(0, 0)
 
-    def axe(self, target: 'BaseChampion') -> None:
-        self.do_damage(target, 1.4 * self.ad + [125, 200, 700][self.level - 1],
+    def axe(self) -> None:
+        log.debug('%s: Axe', self)
+        self.do_damage(self.target, 1.4 * self.ad + [125, 200, 700][self.level - 1],
                        True)
 
-    def do_attack(self, target: 'BaseChampion') -> None:
-        if len(self.axes) > 0 and self.axes[0] + self.AXE_RETURN_SECONDS < GAME_LOOP.game_time:
+    def do_attack(self) -> None:
+        if len(
+                self.axes
+        ) > 0 and self.axes[0] + self.AXE_RETURN_SECONDS < GAME_LOOP.game_time:
+            self.axes.pop()
+            self.axes.append(GAME_LOOP.game_time)
             self.axe()
         else:
-            super().do_attack(target)
+            super().do_attack()
