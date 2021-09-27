e41a722bca5b1f1363eacee5d42f13bfcc479209
Vinícius Frederico <vafrederico@gmail.com>
2021-09-26 17:09:10 -0700

    dummy 1 1599 hp

--- tft/champions/dummy.py
+++ tft/champions/dummy.py
@@ -5,7 +5,7 @@ from tft.stats import BaseChampStats
 
 
 class Dummy(BaseChampion):
-    base_stats = BaseChampStats([1000, 1800, 3240], [80, 144, 259],
+    base_stats = BaseChampStats([1599, 1800, 3240], [80, 144, 259],
                                 armor=60,
                                 mr=60,
                                 aspd=0.8)
